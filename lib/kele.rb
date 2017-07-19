af61f82a07329d4664b63286a46b302929f3991f
Kevin Wood <krwood90@gmail.com>
2017-07-19 20:30:45 +0000

    added mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -19,4 +19,9 @@ class Kele
         response = self.class.get("#{@base_uri}/users/me", headers: { "authorization" => @auth_token })
         @parse_user = JSON.parse(response.body)
     end
+    
+    def get_mentor_availability(mentor_id)
+        response = self.class.get("#{@base_uri}/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+        @mentor_availability = JSON.parse(response.body)
+    end
 end
